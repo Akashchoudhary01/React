2ce4ffc900b8a474a378571a8b2a2b0e0561283b
Akash Choudhary <akashkrchoudhary007@gmail.com>
2025-06-21 21:20:39 +0530

    //

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,11 +1,26 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+
+// import App from './App.jsx'
+
+
+const anotherElement = (
+  <a href='https://google.com' target='_blank'> Visit Google</a>
+)
+
+const reactElement = React.createElement(
+  'a' , {href: 'https://huihui.com' , target :'_blank'} , 
+  'click me to visit huihui'
+
+)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ 
+  reactElement
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
 )
 
 
