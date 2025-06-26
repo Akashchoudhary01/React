5af6cce3ada2405b1f645c58757629bc544e7940
Akash Choudhary <akashkrchoudhary007@gmail.com>
2025-06-26 14:04:43 +0530

    ==+

--- CurrencyConverter/vite.config.js
+++ CurrencyConverter/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/currency-converter/', // use your repo name
+  base: '/CurrencyConverter/', // ✅ leading and trailing slash
   plugins: [react()],
 });
-
